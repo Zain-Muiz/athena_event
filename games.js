257cb4e735c7dd9d5fb47cd37e93eb62349f8449
Zain-Muiz <60746360+Zain-Muiz@users.noreply.github.com>
2021-01-31 23:28:07 +0530

    problem solved

--- games.js
+++ games.js
@@ -102,35 +102,6 @@ socket.on('player', (msg) => {
       onMouseoverSquare: onMouseoverSquare,
       onSnapEnd: onSnapEnd
     };
-    board = ChessBoard('board', cfg);
-   });
-    var cfg = {
-      orientation: color,
-      draggable: true,
-      position: 'start',
-      onDragStart: onDragStart,
-      onDrop: onDrop,
-      onMouseoutSquare: onMouseoutSquare,
-      onMouseoverSquare: onMouseoverSquare,
-      onSnapEnd: onSnapEnd
-    };
-    board = ChessBoard('board', cfg);
-   });
-    var cfg = {
-      orientation: color,
-      draggable: true,
-      position: 'start',
-      onDragStart: onDragStart,
-      onDrop: onDrop,
-      onMouseoutSquare: onMouseoutSquare,
-      onMouseoverSquare: onMouseoverSquare,
-      onSnapEnd: onSnapEnd
-    };
-    board = ChessBoard('board', cfg);
-   });
 
-   var board;
-  
- 
-    
-  
+    board = ChessBoard('board', cfg);
+})
