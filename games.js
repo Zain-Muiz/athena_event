a0190d4bcc3198daf5f7f95749387052a429f7aa
Zain-Muiz <60746360+Zain-Muiz@users.noreply.github.com>
2021-02-01 23:59:36 +0530

    h

--- games.js
+++ games.js
@@ -102,6 +102,36 @@ socket.on('player', (msg) => {
       onMouseoverSquare: onMouseoverSquare,
       onSnapEnd: onSnapEnd
     };
+    var cfg = {
+      orientation: color,
+      draggable: true,
+      position: 'start',
+      onDragStart: onDragStart,
+      onDrop: onDrop,
+      onMouseoutSquare: onMouseoutSquare,
+      onMouseoverSquare: onMouseoverSquare,
+      onSnapEnd: onSnapEnd
+    };
+    var cfg = {
+      orientation: color,
+      draggable: true,
+      position: 'start',
+      onDragStart: onDragStart,
+      onDrop: onDrop,
+      onMouseoutSquare: onMouseoutSquare,
+      onMouseoverSquare: onMouseoverSquare,
+      onSnapEnd: onSnapEnd
+    };
+    var cfg = {
+      orientation: color,
+      draggable: true,
+      position: 'start',
+      onDragStart: onDragStart,
+      onDrop: onDrop,
+      onMouseoutSquare: onMouseoutSquare,
+      onMouseoverSquare: onMouseoverSquare,
+      onSnapEnd: onSnapEnd
+    };
 
     board = ChessBoard('board', cfg);
 })
