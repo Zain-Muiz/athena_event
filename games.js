d7c1e2bcb7595d2893dde7cca4268a1f06ed1ae2
Zain-Muiz <60746360+Zain-Muiz@users.noreply.github.com>
2021-03-05 23:33:29 +0530

    M

--- games.js
+++ games.js
@@ -102,36 +102,7 @@ socket.on('player', (msg) => {
       onMouseoverSquare: onMouseoverSquare,
       onSnapEnd: onSnapEnd
     };
-    var cfg = {
-      orientation: color,
-      draggable: true,
-      position: 'start',
-      onDragStart: onDragStart,
-      onDrop: onDrop,
-      onMouseoutSquare: onMouseoutSquare,
-      onMouseoverSquare: onMouseoverSquare,
-      onSnapEnd: onSnapEnd
-    };
-    var cfg = {
-      orientation: color,
-      draggable: true,
-      position: 'start',
-      onDragStart: onDragStart,
-      onDrop: onDrop,
-      onMouseoutSquare: onMouseoutSquare,
-      onMouseoverSquare: onMouseoverSquare,
-      onSnapEnd: onSnapEnd
-    };
-    var cfg = {
-      orientation: color,
-      draggable: true,
-      position: 'start',
-      onDragStart: onDragStart,
-      onDrop: onDrop,
-      onMouseoutSquare: onMouseoutSquare,
-      onMouseoverSquare: onMouseoverSquare,
-      onSnapEnd: onSnapEnd
-    };
+
 
 
 })
